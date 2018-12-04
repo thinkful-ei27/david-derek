55baf29e1fbe306aef9198b120c482c3e284c4fd
Derek Houck <derek@derekhouck.com>
2018-12-04 09:24:07 -0800

    Add Functions as arguments (1)

---
+++ functionsAsArguments1.js
@@ -0,0 +1,18 @@
+'use strict';
+
+function repeat(fn, n) {
+  for (let i = 0; i < n; i++) {
+    fn();
+  }
+}
+
+function hello() {
+  console.log('Hello world');
+}
+
+function goodbye() {
+  console.log('Goodbye world');
+}
+
+repeat(hello, 5);
+repeat(goodbye, 5);
